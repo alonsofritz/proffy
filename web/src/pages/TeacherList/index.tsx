80171b18d463fab752637c74ebfb63f7cb26e831
Alonso Fritz <alonsofritz@hotmail.com>
2020-08-07 14:51:41 -0300

    feat(frontend): Created static frontend with no backend connection
    Essential features added and styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 
-import "./styles.css"
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import "./styles.css"
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matérias</label>
-                        <input type="text" className="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da Semana</label>
-                        <input type="text" className="week-day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" className="time" />
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Unity', label: 'Unity' },
+                            { value: 'C++', label: 'C++' },
+                            { value: 'ReactJS', label: 'ReactJS' },
+                            { value: 'Javascript', label: 'Javascript' },
+                            { value: 'Node', label: 'Node' }
+                        ]} 
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Dia da Semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-Feira' },
+                            { value: '2', label: 'Terça-Feira' },
+                            { value: '3', label: 'Quarta-Feira' },
+                            { value: '4', label: 'Quinta-Feira' },
+                            { value: '5', label: 'Sexta-Feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]} 
+                    />
+                    <Input type="time" name="time" label="Hora"/>
                 </form>
             </PageHeader>
 
